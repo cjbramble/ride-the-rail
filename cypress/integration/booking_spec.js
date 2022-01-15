61e8253a2fad973d969b68806ccf0748ac8e51cb
cjbramble <email@chetbramble.com>
2022-01-14 21:36:41 -0500

    formatter

--- cypress/integration/booking_spec.js
+++ cypress/integration/booking_spec.js
@@ -1,21 +1,17 @@
 /// <reference types="cypress" />
 
-
-const departureStation = "Lagos"
-const arrivalStation = "Porto - Campanha"
+const departureStation = "Lagos";
+const arrivalStation = "Porto - Campanha";
 
 describe("Can search for a trip", () => {
     it("can search for a route", () => {
-        cy.visit("/")
+        cy.visit("/");
 
         // TODO: Find better element to select
-        cy.get(':nth-child(2) > .btn > .desc-menu')
-          .click()
+        cy.get(":nth-child(2) > .btn > .desc-menu").click();
 
-        cy.get('[name="textBoxPartida"]')
-          .type(departureStation)
+        cy.get('[name="textBoxPartida"]').type(departureStation);
 
-        cy.get('[name="textBoxChegada"]')
-          .type(arrivalStation)
-    })
-})
+        cy.get('[name="textBoxChegada"]').type(arrivalStation);
+    });
+});
