280614e5b5782bab020c0ef485e7b1222cc4d101
cjbramble <email@chetbramble.com>
2022-01-17 18:24:14 -0500

    Stashed Date work in comment.
    Ran formatter to clean up.

--- cypress/integration/booking_spec.js
+++ cypress/integration/booking_spec.js
@@ -3,6 +3,55 @@
 const departureStation = "Lagos";
 const arrivalStation = "Porto - Campanha";
 
+/**
+ * TODO: Move all Date calculataions to a Command once working
+ * - account for different number of days in each month
+ * - account for future date selections being outside of current month
+ * - account for leap year
+ * - leaving for posterity
+ */
+// const months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+// ];
+
+// const daysOfEachMonth = new Map([
+//     ["January", 31],
+//     ["February", 28],
+//     ["March", 31],
+//     ["April", 30],
+//     ["May", 31],
+//     ["June", 30],
+//     ["July", 31],
+//     ["August", 31],
+//     ["September", 30],
+//     ["October", 31],
+//     ["November", 30],
+//     ["December", 31],
+// ]);
+
+// const date = new Date();
+// const month = months[date.getMonth()];
+// const dayOfMonth = date.getDate();
+// const numberOfDays = daysOfEachMonth.get(month);
+// const year = date.getFullYear();
+// const daysUntilDeparture = dayOfMonth + 3;
+// const departureDate = daysUntilDeparture + " " + month + ", " + year;
+// const daysUntilArrival = dayOfMonth + 6;
+// const arrivalDate = daysUntilArrival + " " + month + ", " + year;
+// const departureDelay = 3;
+// const arrivalDelay = 6;
+
 describe("Can search for a trip", () => {
     it("redirects to Buy Tickets interface", () => {
         cy.visit("/");
@@ -25,7 +74,7 @@ describe("Can search for a trip", () => {
     it("selects departure and arrival dates", () => {
         // cy.get('[name="departDate"]').clear().type("22 March, 2022" + '{enter}', {force: true}); // TODO: stop field from clearing
         // cy.get('[name="returnDate"]').clear().type("25 March, 2022" + '{enter}', {force: true}); // TODO: this isn't typing
-        
+
         // TODO: find dynamic method for the following or make above work
         cy.get('[name="departDate"]').click();
         cy.get("#datepicker-first_table")
@@ -38,7 +87,7 @@ describe("Can search for a trip", () => {
     });
 
     it("selects seating class", () => {
-        cy.get("#option1Label").click().should('have.class', 'active');
+        cy.get("#option1Label").click().should("have.class", "active");
     });
 
     it("redirects to confirmation page", () => {
